761da3981d79af3824a3f3ca8fea94fad539dd3c
mavi888 <marcia.villalba@gmail.com>
2024-10-30 14:49:03 +0200

    Adding create order endpoint

--- handler.js
+++ handler.js
@@ -1,9 +1,29 @@
-exports.hello = async (event) => {
+const { v4: uuidv4 } = require('uuid');
+
+exports.newOrder = async (event) => {
+
+  const orderId = uuidv4();
+  console.log(orderId);
+
+  let orderDetails;
+  try {
+    orderDetails = JSON.parse(event.body); 
+  } catch (error) {
+    console.error("Error parsing order details:", error);
     return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Go Serverless v4! Your function executed successfully!",
-      }),
+      statusCode: 400,
+      body: JSON.stringify({ message: "Invalid JSON format in order details" }),
     };
+  }
+
+  console.log(orderDetails)
+
+  const order = {orderId, ...orderDetails}
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: order  
+    }),
   };
-  
+}
